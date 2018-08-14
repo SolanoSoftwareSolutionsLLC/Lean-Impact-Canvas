5c9c962037572b149a3e9d7c7c2939b69843a7b0
Hassam Solano-Morel <HassamSolano@gmail.com>
2018-08-14 18:33:08 -0400

    Trying to fix gitignore

---
+++ LCHelper/User/LCUser.swift
@@ -0,0 +1,9 @@
+//
+//  LCUser.swift
+//  LCHelper
+//
+//  Created by Hassam Solano-Morel on 7/26/18.
+//  Copyright © 2018 SolanoSoftwareSolutionsLLC. All rights reserved.
+//
+
+import Foundation
